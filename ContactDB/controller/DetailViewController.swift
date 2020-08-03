467e183a994e3a5fd9feb449f0c191f868160594
thinhdang <thinhdang@thinhdangs-iMac-Pro.local>
2020-08-03 22:21:44 +0700

    show detail

--- ContactDB/controller/DetailViewController.swift
+++ ContactDB/controller/DetailViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class DetailViewController: UITableViewController {
+    @IBOutlet var DetailTableView: UITableView!
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var positionLabel: UILabel!
@@ -20,86 +21,27 @@ class DetailViewController: UITableViewController {
     var textMobile = String()
     var textPosition = String()
     var textEmail = String()
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        nameLabel?.text = currentContact.name
-        positionLabel?.text = currentContact.position
-        emailButton?.setTitle(currentContact.email, for: .normal)
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        showInfor()
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(handleEdit))
+    }
+    
+
+    @objc func showInfor(){
+       nameLabel?.text = currentContact.name
+       positionLabel?.text = currentContact.position
+       emailButton?.setTitle(currentContact.email, for: .normal)
+       phoneButton?.setTitle(currentContact.phone, for: .normal)
     }
     
     @objc func handleEdit(){
         let vc = storyboard?.instantiateViewController(withIdentifier: "EditViewController") as! EditViewController
         vc.navigationItem.largeTitleDisplayMode = .never
-        navigationController?.pushViewController(vc, animated: true)
+        vc.current = currentContact
+        //navigationController?.pushViewController(vc, animated: true)
+        present(UINavigationController(rootViewController: vc),animated: true)
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
