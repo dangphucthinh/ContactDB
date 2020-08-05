280e1a0bbfe1ddaf73eed753aa74ff9c1fd4671e
thinhdang <thinhdang@thinhdangs-iMac-Pro.local>
2020-08-05 11:27:38 +0700

    Alert Error

--- ContactDB/Utilities/AlertService.swift
+++ ContactDB/Utilities/AlertService.swift
@@ -10,9 +10,12 @@ import Foundation
 import UIKit
 
 class AlertService{
-    private init() {}
     
-    static func deleteAlert(){
-        
+    private init() {}
+    static func errorAlert(in vc: UIViewController, name: String){
+        let alert = UIAlertController(title: "Error", message: "You have to input invalid \(name)", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {action in
+        }))
+        vc.present(alert,animated: true,completion: nil)
     }
 }
---
+++ ContactDB/Utilities/Validation.swift
@@ -0,0 +1,9 @@
+//
+//  Validation.swift
+//  ContactDB
+//
+//  Created by thinhdang on 8/4/20.
+//  Copyright © 2020 thinhdang. All rights reserved.
+//
+
+import Foundation
